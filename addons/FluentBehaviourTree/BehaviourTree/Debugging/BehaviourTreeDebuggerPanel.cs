3ddd8253af552a028c76f337185392b76eef1a55
Cpaz <nicky.shadow99@gmail.com>
2026-05-10 00:55:01 -0400

    hotfix(debug-panel): Added missing linq import

--- addons/FluentBehaviourTree/BehaviourTree/Debugging/BehaviourTreeDebuggerPanel.cs
+++ addons/FluentBehaviourTree/BehaviourTree/Debugging/BehaviourTreeDebuggerPanel.cs
@@ -1,6 +1,7 @@
 ﻿using Godot;
 using Godot.Collections;
 using System.Collections.Generic;
+using System.Linq;
 namespace fluent_behaviour_tree.addons.FluentBehaviourTree.BehaviourTree.Debugging;
 
 [Tool]
